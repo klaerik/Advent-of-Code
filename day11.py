1e7beb2a283dd741a5dbf80109ced999501505dc
Karl Erik <karlos@example.com>
2018-12-13 14:31:18 -0800

    Solve part one for day 11

--- day11.py
+++ day11.py
@@ -7,7 +7,7 @@ def build_grid(serial):
             if x == 0 or y == 0:
                 row.append(None)
             else:
-                rack_id = x * 10
+                rack_id = x + 10
                 power = rack_id * y
                 power += serial
                 power *= rack_id
@@ -17,29 +17,27 @@ def build_grid(serial):
         grid.append(row)
     return(grid)
 
-def get_max_cell(grid):
-    max_cell = 0, 0, 0
+def get_max_cell(grid, fixed):
+    max_cell = 0, 0, 0, 0
     for y in range(1, 298):
         for x in range(1, 298):
             cell = [val for row in grid[y: y+3] for val in row[x: x+3]]
             ttl = sum(cell)
-            if (x, y) in [(33, 45), (21, 61)]:
-                print((x, y, ttl))
-                print(cell)
-            if ttl > max_cell[2]:
-                max_cell = x, y, ttl
+            if ttl > max_cell[3]:
+                max_cell = x, y, len(cell), ttl
     return max_cell
 
-def solve_puzzle(serial):
+def solve_puzzle(serial, fixed=True):
     grid = build_grid(serial)
-    max_cell = get_max_cell(grid)
+    max_cell = get_max_cell(grid, fixed)
     print(f"Max cell for serial {serial} was {max_cell}")
     return max_cell
 
 serial = 73
 solve_puzzle(serial)
 
-#assert 
-solve_puzzle(18) == (33, 45, 29)
-solve_puzzle(42) == (21, 61, 30)
+assert solve_puzzle(18) == (33, 45, 9, 29)
+assert solve_puzzle(42) == (21, 61, 9, 30)
 
+solution = solve_puzzle(9995)
+print(f'Solution 1: {solution}')
