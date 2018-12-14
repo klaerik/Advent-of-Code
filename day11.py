4d5a5224e793b7a46ac0106070d3bfddb001627c
Karl Erik <karlos@example.com>
2018-12-14 14:56:54 -0800

    Solve part 2 of day 11

--- day11.py
+++ day11.py
@@ -19,15 +19,20 @@ def build_grid(serial):
 
 def get_max_cell(grid, fixed):
     max_cell = 0, 0, 0, 0
-    for y in range(1, 298):
-        for x in range(1, 298):
-            cell = [val for row in grid[y: y+3] for val in row[x: x+3]]
-            ttl = sum(cell)
-            if ttl > max_cell[3]:
-                max_cell = x, y, len(cell), ttl
+    if fixed:
+        cell_size = [fixed,]
+    else:
+        cell_size = range(2,101)
+    for size in cell_size:
+        for y in range(1, 301 - size):
+            for x in range(1, 301 - size):
+                cell = [val for row in grid[y: y+size] for val in row[x: x+size]]
+                ttl = sum(cell)
+                if ttl > max_cell[3]:
+                    max_cell = x, y, size, ttl
     return max_cell
 
-def solve_puzzle(serial, fixed=True):
+def solve_puzzle(serial, fixed=3):
     grid = build_grid(serial)
     max_cell = get_max_cell(grid, fixed)
     print(f"Max cell for serial {serial} was {max_cell}")
@@ -40,4 +45,12 @@ assert solve_puzzle(18) == (33, 45, 9, 29)
 assert solve_puzzle(42) == (21, 61, 9, 30)
 
 solution = solve_puzzle(9995)
-print(f'Solution 1: {solution}')
+print(f'Solution 1: {",".join([str(i) for i in solution[:2]])}')
+
+
+assert solve_puzzle(18, fixed=16) == (90, 269, 16, 113)
+assert solve_puzzle(42, fixed=False) == (232, 251, 12, 119)
+
+
+solution2 = solve_puzzle(9995, fixed=False)
+print(f'Solution 2: {",".join([str(i) for i in solution2[:3]])}')
