ca88d1faf8b99597478fb41d3df469e1e282163f
Karl Erik <klaerik@users.noreply.github.com>
2020-12-12 15:25:06 -0800

    Add subclass for fun

--- 2020/day12.py
+++ 2020/day12.py
@@ -9,6 +9,9 @@ class Ship():
         self.x = 0
         self.y = 0
 
+    def loc(self):
+        return self.x, self.y
+
     def turn(self, dir, degrees):
         rotation = {'L': 1, 'R': -1}
         times = degrees // 90
@@ -39,44 +42,29 @@ class Ship():
             self.move(action, dist)
 
 
-class WaypointShip():
+class WaypointShip(Ship):
     def __init__(self, waypoint_x=10, waypoint_y=1):
-        self.x = 0
-        self.y = 0
-        self.wx = waypoint_x
-        self.wy = waypoint_y
+        super().__init__()
+        self.x = waypoint_x
+        self.y = waypoint_y
+        self.ship_x = 0
+        self.ship_y = 0
+    
+    def loc(self):
+        return self.ship_x, self.ship_y
 
     def turn(self, dir, degrees):
         times = degrees // 90
-        for i in range(times):
+        for _ in range(times):
             if dir == 'L':
-                self.wx, self.wy = -self.wy,  self.wx
+                self.x, self.y = -self.y,  self.x
             elif dir == 'R':
-                self.wx, self.wy =  self.wy, -self.wx
-
-    def move(self, dir, dist):
-        if dir == 'N':
-            self.wy += dist
-        elif dir == 'S':
-            self.wy -= dist
-        elif dir == 'W':
-            self.wx -= dist
-        elif dir == 'E':
-            self.wx += dist
+                self.x, self.y =  self.y, -self.x
 
     def forward(self, dist):
-        self.x += self.wx * dist
-        self.y += self.wy * dist
+        self.ship_x += self.x * dist
+        self.ship_y += self.y * dist
 
-    def step(self, cmd):
-        action = cmd[0]
-        dist = int(cmd[1:])
-        if action in ('L','R'):
-            self.turn(action, dist)
-        elif action == 'F':
-            self.forward(dist)
-        else:
-            self.move(action, dist)
 
 def manhattan_dist(x, y):
     return abs(x) + abs(y)
@@ -85,7 +73,7 @@ def traverse_commands(commands, model):
     ship = model()
     for command in commands:
         ship.step(command)
-    return manhattan_dist(ship.x, ship.y)
+    return manhattan_dist(*ship.loc())
 
 
 # Solve puzzle
