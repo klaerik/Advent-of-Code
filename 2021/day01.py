499150d1e5b5e3dcbdef09338f0a8f8f9d558b86
Karl Erik <klaerik@users.noreply.github.com>
2021-11-30 22:03:24 -0800

    Clean up formatting for day 1

--- 2021/day01.py
+++ 2021/day01.py
@@ -1,6 +1,10 @@
 from shared import read_file
 from collections import deque
 
+
+# Data
+raw = read_file('input/day01.txt', convert=int)
+
 test = [
     199,
     200,
@@ -15,6 +19,7 @@ test = [
     ]
 
 
+# Functions
 def track_depth(raw):
     out = 0
     last = None
@@ -25,33 +30,26 @@ def track_depth(raw):
         last = depth
     return out
 
-def track_depth_sliding(raw, window=3):
-    nums = deque([])
+def sliding_window(raw, window):
+    nums = deque()
     current = 0
-    last = None
-    out = 0
+    out = []
     for depth in raw:
         nums.append(depth)
         current += depth
         if len(nums) == window:
-            if last and current > last:
-                out += 1
-            last = current
+            out.append(current)
             current -= nums.popleft()
     return out
 
-
-assert track_depth(test) == 7
+def track_depth_sliding(raw, window=3):
+    return track_depth(sliding_window(raw, window))
 
 
 ### Solve part 1
-raw = read_file('input/day01.txt', convert=int)
-
-
+assert track_depth(test) == 7
 print(f'Part 1: Depth increased {track_depth(raw)} times')
 
-
-assert track_depth_sliding(test) == 5
-
 ### Solve part 2
-print(f'Part 2: Depth increased {track_depth_sliding(raw)} times with sliding window')
+assert track_depth_sliding(test) == 5
+print(f'Part 2: Depth increased {track_depth_sliding(raw)} times with sliding window of 3')
