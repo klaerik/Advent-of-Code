d1c5bf4bbb5a670e461348d00efcf080f64b2cbb
Karl Erik <klaerik@users.noreply.github.com>
2021-12-01 08:01:42 -0800

    Tidy up function for day 1

--- 2021/day01.py
+++ 2021/day01.py
@@ -5,6 +5,7 @@ from collections import deque
 # Data
 raw = read_file('input/day01.txt', convert=int)
 
+
 test = [
     199,
     200,
@@ -20,21 +21,23 @@ test = [
 
 
 # Functions
-def track_depth(raw):
+def track_depth(depths, window=None):
+    if window:
+        depths = sliding_window(depths, window)
     out = 0
     last = None
-    for depth in raw:
+    for depth in depths:
         if last:
             if last < depth:
                 out += 1
         last = depth
     return out
 
-def sliding_window(raw, window):
+def sliding_window(depths, window):
     nums = deque()
     current = 0
     out = []
-    for depth in raw:
+    for depth in depths:
         nums.append(depth)
         current += depth
         if len(nums) == window:
@@ -42,14 +45,11 @@ def sliding_window(raw, window):
             current -= nums.popleft()
     return out
 
-def track_depth_sliding(raw, window=3):
-    return track_depth(sliding_window(raw, window))
-
-
 ### Solve part 1
 assert track_depth(test) == 7
 print(f'Part 1: Depth increased {track_depth(raw)} times')
 
 ### Solve part 2
-assert track_depth_sliding(test) == 5
-print(f'Part 2: Depth increased {track_depth_sliding(raw)} times with sliding window of 3')
+window = 3
+assert track_depth(test, window) == 5
+print(f'Part 2: Depth increased {track_depth(raw, window)} times with sliding window of {window}')
