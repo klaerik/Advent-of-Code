40e03941f352e166d6e7f63904ea4f65947e175e
klaerik <klaerik@users.noreply.github.com>
2022-12-06 07:53:27 -0800

    Add solution that is more efficient

--- y2022/day06.py
+++ y2022/day06.py
@@ -1,5 +1,6 @@
 import y2022.shared as shared
-from collections import deque
+from collections import deque, Counter
+
 
 ## Data
 raw = shared.read_file('day06.txt')[0]
@@ -15,6 +16,19 @@ def solve(raw, msg_len=4):
             return i
     return None
 
+def solve_better(raw, msg_len=4):
+    seen = deque()
+    counts = Counter()
+    for i,val in enumerate(raw, start=1):
+        seen.append(val)
+        counts[val] += 1
+        if len(seen) > msg_len:
+            counts[seen.popleft()] -= 1
+            if counts.most_common(1)[0][1] == 1:
+                return i
+    return None
+
+
 ## Testing
 assert solve('bvwbjplbgvbhsrlpgdmjqwftvncz') == 5
 assert solve('mjqjpqmgbljsphdztnvjfqwrcgsmlb', msg_len=14) == 19
@@ -22,3 +36,4 @@ assert solve('mjqjpqmgbljsphdztnvjfqwrcgsmlb', msg_len=14) == 19
 ## Solutions
 print(f"Solution to part 1: {solve(raw)}")
 print(f"Solution to part 2: {solve(raw, msg_len=14)}")
+
