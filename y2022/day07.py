62e52aff51f6e9bc7b995cf2f32bc6f6c7f33fc5
klaerik <klaerik@users.noreply.github.com>
2022-12-07 07:21:59 -0800

    Initial solution for day 7

---
+++ y2022/day07.py
@@ -0,0 +1,144 @@
+import y2022.shared as shared
+from dataclasses import dataclass
+from typing import List, Dict
+
+## Data
+raw = shared.read_file('day07.txt')
+test = shared.read_file('day07-test.txt')
+
+## Functions
+@dataclass
+class Directory:
+    name: str
+    parent: dict
+    dirs: Dict[str, "Directory"] = None
+    files: Dict[str, "File"] = None
+    size: int = 0
+
+@dataclass
+class File:
+    name: str
+    size: int = 0
+    ext: str = None
+
+@dataclass
+class FileSystem:
+    commands: List[str]
+    root: Directory = None
+    cwd: Directory = None
+    dirs: list = None
+
+    def __post_init__(self):
+        self.cwd = Directory(
+            name = 'root',
+            files = {},
+            dirs = {},
+            parent = None,
+        )
+        self.root = self.cwd
+        self.dirs = []
+
+    def is_command(self, line: str):
+        return line.startswith('$')
+
+    def split_command(self, line: str):
+        command = line[2:]
+        if ' ' in command:
+            command, arg = command.split()
+        else:
+            arg = None
+        return command, arg
+
+    def process_line(self, line: str):
+        if line.startswith('dir'):
+            dirname = line.split()[1]
+            if dirname == '/':
+                dirname = 'root'
+            if dirname not in self.cwd.dirs:
+                new_dir = Directory(
+                    name = dirname,
+                    parent = self.cwd,
+                    dirs = {},
+                    files = {},
+                )
+                self.cwd.dirs[dirname] = new_dir
+        elif line[0].isdigit():
+            size, filename = line.split()
+            if filename not in self.cwd.files:
+                new_file = File(
+                    name = filename,
+                    # parent = self.cwd,
+                    size = int(size),
+                    ext = filename.split('.')[1] if '.' in filename else None
+                )
+                self.cwd.files[filename] = new_file
+        elif self.is_command(line):
+            command, arg = self.split_command(line)
+            if command == 'cd' and arg != '/':
+                if arg == '..':
+                    self.cwd = self.cwd.parent
+                else:
+                    if arg not in self.cwd.dirs:
+                        print(f"Creating directory {arg}...")
+                        new_dir = Directory(
+                            name = arg,
+                            parent = self.cwd,
+                            dirs = {},
+                            files = {},
+                        )
+                        self.cwd.dirs[arg] = new_dir
+                    self.cwd = self.cwd.dirs[arg]
+            elif command == 'ls':
+                pass
+    
+    def process_lines(self):
+        for line in self.commands:
+            self.process_line(line)
+
+    def calc_dir_size(self, directory: Directory = None):
+        if directory is None:
+            directory = self.root
+        size = 0
+        size += sum([file.size for file in directory.files.values()])
+        size += sum([self.calc_dir_size(subdir) for subdir in directory.dirs.values()])
+        directory.size = size
+        self.dirs.append(directory)
+        return size
+    
+    def get_total_size(self, max_size=100000):
+        return sum([d.size for d in self.dirs if d.size <= max_size])
+
+    def find_dir_to_delete(self, 
+            fs_max_size = 70000000,
+            update_size = 30000000,
+            ):
+        _ = self.get_total_size()
+        remaining_size = fs_max_size - self.root.size
+        free_up_size = update_size - remaining_size
+        if free_up_size <= 0:
+            return 0
+        else:
+            sizes = [d.size for d in self.dirs if d.size >= free_up_size]
+            sizes.sort()
+            return sizes[0]
+
+def solve(raw):
+    fs = FileSystem(raw)
+    fs.process_lines()
+    fs.calc_dir_size()
+    return fs.get_total_size()
+
+def solve2(raw):
+    fs = FileSystem(raw)
+    fs.process_lines()
+    fs.calc_dir_size()
+    return fs.find_dir_to_delete()
+
+## Testing
+assert solve(test) == 95437
+assert solve2(test) == 24933642
+
+
+## Solutions
+print(f"Solution to part 1: {solve(raw)}")
+print(f"Solution to part 2: {solve2(raw)}")
