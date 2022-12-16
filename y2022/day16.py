e590a88d0637bd333e726a75e3bda19f442f4d0f
klaerik <klaerik@users.noreply.github.com>
2022-12-16 07:58:55 -0800

    Start on day 16

---
+++ y2022/day16.py
@@ -0,0 +1,26 @@
+import y2022.shared as shared
+from dataclasses import dataclass, field
+
+## Data
+raw = shared.read_file("day16.txt")
+test = shared.read_file("day16-test.txt")
+
+## Functions
+
+
+def solve(raw):
+    pass
+
+
+def solve2(raw):
+    pass
+
+
+## Testing
+assert solve(test) == None
+assert solve2(test) == None
+
+
+## Solutions
+print(f"Solution to part 1: {solve(raw)}")
+print(f"Solution to part 2: {solve2(raw)}")
